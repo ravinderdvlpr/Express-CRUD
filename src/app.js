930489c4845739960d36d85cd2ff548d1d481059
Lakhtar Singh <kaupa_lakhtar@os4w.de>
2022-11-02 12:19:00 +0530

    First Commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = require("./routes");
+const mongoose = require("mongoose");
+const app = express()
+const port = 3000
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
